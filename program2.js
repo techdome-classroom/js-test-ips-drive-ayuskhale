a2fea991f3bdd7e71518cd26bbf43025dda5d099
ManyaS1011 <aynam1010@gmail.com>
2024-04-26 12:51:28 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,26 +1,7 @@
 function longestSubstring(s) {
-    if (!s) return 0;
 
-    let charIndex = {};
-    let maxLength = 0;
-    let start = 0;
 
-    for (let end = 0; end < s.length; end++) {
-        if (s[end] in charIndex && charIndex[s[end]] >= start) {
-            start = charIndex[s[end]] + 1;
-        } else {
-            maxLength = Math.max(maxLength, end - start + 1);
-        }
-        charIndex[s[end]] = end;
-    }
-
-    return maxLength;
+    // Implementation of longestSubstring function
 }
 
 module.exports = { longestSubstring };
-
-// Example usage:
-// const { longestSubstring } = require('./longestSubstring');
-// const inputStr = "abcabcbb";
-// console.log(longestSubstring(inputStr)); // Output: 3
-

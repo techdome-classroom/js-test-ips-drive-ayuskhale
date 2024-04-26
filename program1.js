5490a7c5481dd02514a8e69fa4ea540bfe7266ed
ManyaS1011 <aynam1010@gmail.com>
2024-04-26 12:43:47 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,10 @@
 function smallestMissingPositiveInteger(nums) {
-  // Remove non-positive integers
+
   nums = nums.filter(num => num > 0);
   
-  // usinSort the array
   nums.sort((a, b) => a - b);
   
-  // Iterate through the sorted array to find the smallest missing positive integer
+  
   let smallestMissing = 1;
   for (const num of nums) {
       if (num === smallestMissing) {

2be9f4a821b114cd5beac1ef49458e790ccd7022
ManyaS1011 <aynam1010@gmail.com>
2024-04-26 13:01:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,20 +1,20 @@
 function smallestMissingPositiveInteger(nums) {
-  let n = nums.length;
-  let present = new Array(n + 1).fill(false);
+    let n = nums.length;
+    let present = new Array(n + 1).fill(false);
 
-  for (let i = 0; i < n; i++) {
-      if (nums[i] > 0 && nums[i] <= n) {
-          present[nums[i]] = true;
-      }
-  }
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0 && nums[i] <= n) {
+            present[nums[i]] = true;
+        }
+    }
 
-  for (let i = 1; i <= n; i++) {
-      if (!present[i]) {
-          return i;
-      }
-  }
+    for (let i = 1; i <= n; i++) {
+        if (!present[i]) {
+            return i;
+        }
+    }
 
-  return n + 1;
+    return n + 1;
 }
 
 module.exports = smallestMissingPositiveInteger;
